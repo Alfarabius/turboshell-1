000d37f263ae8ee01cca054c71face655b6b0c5c
Malborn Rosie <mrosie@ge-f4.kzn.21-school.ru>
2021-04-01 17:00:13 +0300

    add loop

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mrosie <mrosie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 15:25:30 by mrosie            #+#    #+#             */
-/*   Updated: 2021/04/01 16:17:55 by mrosie           ###   ########.fr       */
+/*   Updated: 2021/04/01 16:59:14 by mrosie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	char buf[1024];
+	char	buf[1024];
+	char	shell_is_running;
 
+	shell_is_running = 1;
 	(void)argc;
 	(void)argv;
 	(void)env;
-	write(1, YELLOW, 6);
-	write(1, "turboshell-1.0$ ", 17);
-	write(1, STANDART, 5);
-	read(0, buf, 1024);
+	while (shell_is_running)
+	{
+		write(1, YELLOW, 6);
+		write(1, "turboshell-1.0$ ", 17);
+		write(1, STANDART, 5);
+		read(0, buf, 1024);
+	}
 	return (0);
 }
