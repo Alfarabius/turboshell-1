2b35042ed8d865c261a270debdc3945ea060cfdb
Pinkman Lurlene <plurlene@ge-g4.kzn.21-school.ru>
2021-04-02 18:03:58 +0300

    parser v1.1

--- parser.c
+++ parser.c
@@ -6,22 +6,27 @@
 /*   By: plurlene <plurlene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 18:27:59 by plurlene          #+#    #+#             */
-/*   Updated: 2021/04/01 20:19:22 by plurlene         ###   ########.fr       */
+/*   Updated: 2021/04/02 17:54:50 by plurlene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
-static char *ft_strndup(char *str, int n)
+void clear_arr(char ***arr)
 {
-	char *res;
-	char *temp;
+	int i;
 
-	temp = ft_strdup(str);
-	temp[n] = '\0';
-	res = ft_strdup(temp);
-	free(temp);
-	return (res);
+	i = -1;
+	if (*arr)
+	{
+		while ((*arr)[++i])
+		{
+
+			free((*arr)[i]);
+		}
+		free(*arr);
+		*arr = NULL;
+	}
 }
 
 static char *ft_clear_strdup(char *str)
@@ -33,7 +38,7 @@ static char *ft_clear_strdup(char *str)
 	return (res);
 }
 
-static void add_line(char ***arr, char *line, int n)
+static void add_line(char ***arr, char *line)
 {
 	int		len;
 	char	**result_arr;
@@ -43,37 +48,78 @@ static void add_line(char ***arr, char *line, int n)
 		len++;
 	result_arr = (char **)malloc(sizeof(char *) * (len + 2));
 	result_arr[len + 1] = NULL;
-	result_arr[len] = ft_strndup(line, n);
+	result_arr[len] = ft_strdup(line);
 	while (--len >= 0)
 		result_arr[len] = ft_clear_strdup((*arr)[len]);
 	free(*arr);
 	*arr = result_arr;
 }
 
+int skip_whitespaces(char *str, int i)
+{
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	return (i);
+}
+
+// NULL to the pointer after free
+static void ft_free(void **bfree)
+{
+	if (*bfree)
+		free(*bfree);
+	*bfree = NULL;
+}
+
+static char *ft_realloc(char *str, int num, int c)
+{
+	char	*res;
+	int		len;
+	int		i;
+
+	len =  ft_strlen(str);
+	len += num + 1;
+	res = (char *)malloc(len);
+	res[len - 1] = '\0';
+	i = -1;
+	while (str[++i])
+		res[i] = str[i];
+	while (i < len - 1)
+	{
+		res[i] = c;
+		i++;
+	}
+	ft_free((void **)&str);
+	return (res);
+}
+
 void line_parser(char *str)
 {
-/*--- really specsigns : " $\"\'" ---*/
-//	char *specsigns = " ";
 	char **args;
+	char *current_arg;
 	int i;
-	int n;
 
-	(void) str;
 	args = (char **)malloc(sizeof(char *));
 	args[0] = NULL;
-	i = 0;
-	while (str[i])
+	current_arg = (char *)malloc(1);
+	current_arg[0] = '\0';
+	i = skip_whitespaces(str, 0) - 1;
+	while (str[++i])
 	{
-		n = 0;
-		while (str[i] && str[i] == ' ')
-			i++;
-		n = i;
-		while (str[n] && str[n] != ' ')
-			n++;
-		add_line(&args, &str[i], n - i);
-		i = n;
+		if (str[i] != ' ' && str[i] != '\n') //воткнуть свич кейс на спец символы и кавычки
+			current_arg = ft_realloc(current_arg, 1, str[i]);
+		else
+		{
+			i = skip_whitespaces(str, i) - 1;
+			add_line(&args, current_arg);
+			ft_free((void **)&current_arg);
+			current_arg = (char *)malloc(1);
+			current_arg[0] = '\0';
+		}
 	}
 	i = -1;
 	while (args[++i])
 		printf("args: %s\n", args[i]);
+	clear_arr(&args);
+	if (current_arg)
+		free(current_arg);
 }
