1ebb176f065d7b3eec4dcc7719387b776102b0f3
Malborn Rosie <mrosie@ge-f4.kzn.21-school.ru>
2021-04-07 13:41:44 +0300

    ctrl_d normal work

--- termcap.c
+++ termcap.c
@@ -1,22 +1,17 @@
 #include "minishell.h"
 
-int	term_clean_up(void)
+int	add_to_history(t_tsh tsh)
 {
-	tputs(restore_cursor, 1, ft_putchar);
-	tputs(tigetstr("ed"), 1, ft_putchar);
 	return (0);
 }
 
 int	termcap_processor(char *line, t_tsh *tsh)
 {
+
 	if (!ft_strcmp(line, "\033[A"))
 		history_up(tsh);
 	else if (!ft_strcmp(line, "\033[B"))
 		history_down(tsh);
-	else if (!ft_strcmp(line, "\033[C"))
-		move_right_line(tsh);
-	else if (!ft_strcmp(line, "\033[D"))
-		move_left_line(tsh);
 	else if (!ft_strcmp(line, "\177"))
 		erase_symbol(tsh);
 	return (0);
