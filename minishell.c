d5e21e5a9308330112649582eb9441b38bcebc9f
Malborn Rosie <mrosie@ge-f4.kzn.21-school.ru>
2021-04-14 14:33:21 +0300

    try new history

---
+++ minishell.c
@@ -0,0 +1,88 @@
+#include "minishell.h"
+
+static	int	ctrl_d(t_tsh *tsh)
+{
+	free(tsh->line);
+	tsh->end_line = 1;
+	tsh->line = ft_strdup("exit");
+	if (!tsh->line)
+		return(error_handler("memmory doesn't allocated", 0));
+	return(0);
+}
+
+static	int	init_shell(t_tsh *tsh)
+{
+	char *term_type;
+
+	term_type = getenv("TERM");
+	tcgetattr(0, &tsh->term);
+	tsh->term.c_lflag &= ~(ECHO|ICANON|ISIG);
+	tsh->term.c_cc[VMIN] = 1;
+	tsh->term.c_cc[VTIME] = 0;
+	tcsetattr(0, TCSANOW, &tsh->term);
+	tgetent(0, TERM_NAME); // for DEBUG!!!
+	// if (!term_type || tgetent(0, term_type) != 1) // term_type -> TERM  term_type -> !term_type
+	// 	return (error_handler("Can not find terminal or termcap base."));
+	tsh->hfd = open("tsh_history", O_CREAT | O_RDWR | O_APPEND, 0755);
+	if (tsh->hfd == -1)
+		return(error_handler("history file doesn't open", 1));
+	file_to_history(tsh);
+	tsh->is_running = 1;
+	tsh->symbols = 0;
+	tsh->end_line = 0;
+	tsh->line = (char *)malloc(1);
+	if (!tsh->line)
+		return(error_handler("memmory doesn't allocated", 1));
+	tsh->line[0] = '\0';
+	return (0);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_tsh	tsh;
+	char	*tmp;
+
+	(void)argc;
+	(void)argv;
+	init_shell(&tsh);
+	env_to_lst(&tsh.env, env);
+	// while(tsh.env) // debug
+	// {
+	// 	printf("%s=",((t_dict *)(tsh.env->content))->key);
+	// 	printf("%s\n",((t_dict *)(tsh.env->content))->value);
+	// 	tsh.env = tsh.env->next;
+	// }
+	while (tsh.is_running)
+	{
+		write(1, TSH_NAME, 27);
+		tputs(save_cursor, 1, ft_putchar);
+		tsh.his_ptr = ft_dlstlast(tsh.his);
+		while (!tsh.end_line)
+		{
+			tsh.is_termcap = 0;
+			tsh.symbols = read(0, tsh.buf, 1024);
+			tsh.buf[tsh.symbols] = '\0';
+			write(1, tsh.buf, tsh.symbols);
+			if (tsh.buf[tsh.symbols - 1] == '\n')
+				tsh.end_line = 1;
+			termcap_processor(tsh.buf, &tsh);
+			if(!tsh.is_termcap)
+			{
+				tmp = ft_strjoin(tsh.line, tsh.buf); // refactor
+				free(tsh.line);
+				tsh.line = tmp;
+			}
+			ft_bzero(tsh.buf, 1024);
+			if (!ft_strcmp(tsh.line, "\4"))
+				ctrl_d(&tsh);
+		}
+		if (ft_strcmp(tsh.line, "\n"))
+		{
+			line_parser(tsh);
+			add_to_history(&tsh);
+		}
+		ft_bzero(tsh.line, ft_strlen(tsh.line));
+		tsh.end_line = 0;
+	}
+	return (0);
+}
---
+++ minishell.h
@@ -0,0 +1,70 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# define TERM_NAME "xterm-256color"
+# define TSH_NAME "\x1B[33m turboshell-1.0$ \x1B[0m"
+# define BUFFER_SIZE 1024
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include "./Libft/libft.h"
+# include <term.h>
+# include <curses.h>
+# include <stdlib.h>
+# include <signal.h>
+
+typedef	struct	s_gnl
+{
+	char		*buf;
+	ssize_t		bwr;
+	int			fd;
+}				t_gnl;
+
+typedef	struct		s_prsr
+{
+	char			**args;
+	int				l_index;
+	int				current_arg;
+	int				parse_status;
+}					t_prsr;
+
+typedef	struct		s_dict
+{
+	char			*key;
+	char			*value;
+	char			is_separ;
+	char			is_set;
+}					t_dict;
+
+typedef	struct		s_msh
+{
+	struct	termios	term;
+	t_list			*env;
+	t_dlst			*his;
+	t_dlst			*his_ptr;
+	char			buf[1024];
+	char			*line;
+	char			is_running;
+	char			end_line;
+	char			is_termcap;
+	int				symbols;
+	int				hfd;
+}					t_tsh;
+
+void				ft_exit(void);
+void				line_parser(t_tsh tsh);
+int					env_to_lst(t_list **lst, char **env);
+int					error_handler(char *msg, char flg);
+int					termcap_processor(char *line, t_tsh *tsh);
+int					history_up(t_tsh *tsh);
+int					history_down(t_tsh *tsh);
+int					erase_symbol(t_tsh *tsh);
+int					add_to_history(t_tsh *tsh);
+int					ft_putint(int c);
+int					cmd_processor(t_prsr *prsr);
+int					file_to_history(t_tsh *tsh);
+int					get_next_line(int fd, char **line);
+char				*ft_newreminder(char *rem, char *err);
+char				*ft_strjoin_gnl(char *s1, char *s2);
+char				*ft_nextline(char *rem);
+
+#endif
