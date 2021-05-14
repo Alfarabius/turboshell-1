f4fd05493084659903e65283bd292c775695d005
Malborn Rosie <mrosie@un-l1.kzn.21-school.ru>
2021-05-14 16:21:28 +0300

    binary_processor to norm

--- binary_processor.c
+++ binary_processor.c
@@ -64,13 +64,35 @@ static	int	binary_in_dir(char *path, char *bin)
 	return (0);
 }
 
+static	void	open_binary(t_tsh *tsh, char *binary_path)
+{
+	int		i;
+	pid_t	pid;
+
+	i = -1;
+	binary_path = ft_realloc(binary_path, 1, '/');
+	while (tsh->prsr.args[0] && tsh->prsr.args[0][++i])
+		binary_path = ft_realloc(binary_path, 1, tsh->prsr.args[0][i]);
+	if (!tsh->prsr.pipe.count)
+	{
+		pid = fork();
+		if (!pid)
+		{
+			execve(binary_path, tsh->prsr.args, tsh->env_arr);
+			exit(1);
+		}
+		else
+			waitpid(pid, &g_exit_status, 0);
+	}
+	else
+		execve(binary_path, tsh->prsr.args, tsh->env_arr);
+}
+
 int	binary_processor(t_tsh *tsh)
 {
 	char	*binary_path;
 	int		current_path;
 	int		len;
-	int		i;
-	pid_t	pid;
 	void	*buf;
 
 	current_path = 0;
@@ -80,25 +102,7 @@ int	binary_processor(t_tsh *tsh)
 	{
 		binary_path = get_bpath(current_path++, tsh);
 		if (binary_in_dir(binary_path, tsh->prsr.args[0]))
-		{
-			binary_path = ft_realloc(binary_path, 1, '/');
-			i = -1;
-			while (tsh->prsr.args[0] && tsh->prsr.args[0][++i])
-				binary_path = ft_realloc(binary_path, 1, tsh->prsr.args[0][i]);
-			if (!tsh->prsr.pipe.count)
-			{
-				pid = fork();
-				if (!pid)
-				{
-					execve(binary_path, tsh->prsr.args, tsh->env_arr);
-					exit(1);
-				}
-				else
-					waitpid(pid, &g_exit_status, 0);
-			}
-			else
-				execve(binary_path, tsh->prsr.args, tsh->env_arr);
-		}
+			open_binary(tsh, binary_path);
 		ft_freen((void **)&binary_path);
 	}
 	return (0);
