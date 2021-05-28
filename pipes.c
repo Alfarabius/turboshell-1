958f544fa5e8a5d8208daff11702ef34c1b2061c
Pinkman Lurlene <plurlene@ge-e1.kzn.21-school.ru>
2021-05-28 19:21:22 +0300

    protect forks in pipes.c

--- pipes.c
+++ pipes.c
@@ -1,5 +1,34 @@
 #include "minishell.h"
 
+static void	close_pipes_fd(t_tsh *tsh)
+{
+	close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][1]);
+	close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][0]);
+}
+
+static void	wait_error_pipes(t_tsh *tsh)
+{
+	int	status;
+
+	dup2(tsh->original_fd[1], 1);
+	dup2(tsh->original_fd[0], 0);
+	close_pipes_fd(tsh);
+	(error_template_prsr("turboshell-1.0: ", "", strerror(errno), tsh));
+	tsh->prsr.pipe.current = 0;
+	while (tsh->prsr.pipe.count)
+	{
+		wait(&status);
+		exit_status_handler(status);
+		tsh->prsr.pipe.count--;
+	}
+}
+
+static void	child_handler(t_tsh *tsh, int status)
+{
+	cmd_processor(tsh);
+	exit(status);
+}
+
 void	pipe_processor(t_tsh *tsh)
 {
 	pid_t	pid;
@@ -7,34 +36,17 @@ void	pipe_processor(t_tsh *tsh)
 	pipe(tsh->prsr.pipe.fd[tsh->prsr.pipe.current]);
 	pid = fork();
 	if (pid < 0)
-	{
-		dup2(tsh->original_fd[1], 1);
-		dup2(tsh->original_fd[0], 0);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][1]);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][0]);
-		(error_template_prsr("turboshell-1.0: ", "", strerror(errno), tsh));
-		tsh->prsr.pipe.current = 0;
-		while (tsh->prsr.pipe.count)
-		{
-			wait(&g_exit_status);
-			exit_status_handler(pid);
-			tsh->prsr.pipe.count--;
-		}
-		return ;
-	}
+		return (wait_error_pipes(tsh));
 	if (!pid)
 	{
 		dup2(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][1], 1);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][0]);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][1]);
-		cmd_processor(tsh);
-		exit(0);
+		close_pipes_fd(tsh);
+		child_handler(tsh, 0);
 	}
 	else
 	{
 		dup2(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][0], 0);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][1]);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][0]);
+		close_pipes_fd(tsh);
 	}
 	tsh->prsr.pipe.current++;
 }
@@ -47,19 +59,15 @@ void	wait_pipes(t_tsh *tsh)
 	pipe(tsh->prsr.pipe.fd[tsh->prsr.pipe.current]);
 	pid = fork();
 	if (pid < 0)
-		return (error_template_prsr("turboshell-1.0: ", "", strerror(errno), tsh));
+		return (wait_error_pipes(tsh));
 	tsh->prsr.pipe.count++;
 	dup2(tsh->original_fd[1], 1);
 	if (!pid)
-	{
-		cmd_processor(tsh);
-		exit(g_exit_status);
-	}
+		child_handler(tsh, g_exit_status);
 	else
 	{
 		dup2(tsh->original_fd[0], 0);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][1]);
-		close(tsh->prsr.pipe.fd[tsh->prsr.pipe.current][0]);
+		close_pipes_fd(tsh);
 		tsh->prsr.pipe.current = 0;
 		while (tsh->prsr.pipe.count)
 		{
