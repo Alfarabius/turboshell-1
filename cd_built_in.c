4fa800e4976a7d2632fbc4d680f65614c17ec349
Malborn Rosie <mrosie@pr-h2.kzn.21-school.ru>
2021-06-01 17:31:39 +0300

    fix cd

--- cd_built_in.c
+++ cd_built_in.c
@@ -9,20 +9,39 @@ void	builtin_error(char *msg, char *name, char *btin)
 	g_exit_status = 1;
 }
 
-void	change_oldpwd(int cond, t_tsh *tsh, t_dict **old_pwd, t_dict *pwd)
+static	void	change_oldpwd(int c, t_tsh *tsh, t_dict **opwd, t_dict *pwd)
 {
-	if (cond)
+	if (c)
 	{
-		(*old_pwd)->value = ft_strjoin("OLDPWD=", pwd->value);
-		error_checker(!(*old_pwd)->value, "memmory doesn't allocated", 1);
-		elem_to_lst((*old_pwd)->value, &tsh->env);
+		(*opwd)->value = ft_strjoin("OLDPWD=", pwd->value);
+		error_checker(!(*opwd)->value, "memmory doesn't allocated", 1);
+		elem_to_lst((*opwd)->value, &tsh->env);
 	}
 	else
 	{
-		ft_freen((void **)&((*old_pwd)->value));
-		(*old_pwd)->value = ft_strdup(pwd->value);
-		error_checker(!(*old_pwd)->value, "memmory doesn't allocated", 1);
+		ft_freen((void **)&((*opwd)->value));
+		(*opwd)->value = ft_strdup(pwd->value);
+		error_checker(!(*opwd)->value, "memmory doesn't allocated", 1);
 	}
+}
+
+static	void	change_pwd(t_dict **pwd)
+{
+	char	*err;
+	char	buf[PATH_MAX];
+
+	if ((*pwd)->value)
+		ft_freen ((void **)&(*pwd)->value);
+	err = getcwd(buf, PATH_MAX);
+	if (!err)
+	{
+		err = ft_strjoin("cd: error retrieving current directory: \
+getcwd: cannot access parent directories: ", strerror(errno));
+		ft_putendl_fd(err, 2);
+		free(err);
+	}
+	(*pwd)->value = ft_strdup(buf);
+	error_checker(!(*pwd)->value, "memmory doesn't allocated", 1);
 }
 
 void	ft_cd(t_tsh *tsh, char *dir)
@@ -30,7 +49,6 @@ void	ft_cd(t_tsh *tsh, char *dir)
 	t_list	*current;
 	t_dict	*pwd;
 	t_dict	*old_pwd;
-	char	buf[PATH_MAX];
 
 	current = tsh->env;
 	old_pwd = NULL;
@@ -39,18 +57,16 @@ void	ft_cd(t_tsh *tsh, char *dir)
 		dir = get_env_value(*tsh, "HOME");
 		if (!dir)
 			return (builtin_error("HOME not set", "", "cd: "));
+		if (*dir == 0)
+			return ;
 	}
 	current = tsh->env;
 	if (chdir(dir) == -1)
 		return (builtin_error(": No such file or directory", dir, "cd: "));
 	pwd = get_env_elem(*tsh, "PWD");
 	old_pwd = get_env_elem(*tsh, "OLDPWD");
-	if (!pwd || !old_pwd || !dir)
+	if (!pwd || !old_pwd)
 		return ;
 	change_oldpwd(!old_pwd->value, tsh, &old_pwd, pwd);
-	if (pwd->value)
-		ft_freen ((void **)&pwd->value);
-	getcwd(buf, PATH_MAX);
-	pwd->value = ft_strdup(buf);
-	error_checker(!pwd->value, "memmory doesn't allocated", 1);
+	change_pwd(&pwd);
 }
